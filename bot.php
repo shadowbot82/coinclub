a98b9bcf1782bc7b2cd9589017e9d1fbc2dcfa2c
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-17 15:48:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,6 +8,7 @@ $putih="\033[1;37m";
 $pink="\033[1;35m";
 $red="\033[1;31m";
 $kuning="\033[1;33m";
+$t="\n";
 
 date_default_timezone_set("Asia/Jakarta");
 require_once("sdata-modules.php");
@@ -19,9 +20,9 @@ print $ijo."
 | |___| || |  |   /|  | |___| |___ _| _  \
 |_____|____|__|__/ |__|_____|______||____/\n\n";
 echo $kuning."Bot Coin Club (aplikasi penghasil BTC,ETH,EOS,ICH,BCH) \n";
-echo $putih."======================================= \n";
-echo $kuning."[".$putih."created : ".$ijo."Shadow Bot".$kuning."]".$putih." || code invite : ".$ijo."WK2XZO \n\n";
-echo $putih."======================================= \n";
+echo $putih."=================================================== \n";
+echo $kuning."[".$putih."created : ".$ijo."Shadow Bot".$kuning."]".$putih." || code invite : ".$ijo."WK2XZO \n";
+echo $putih."==================================================== \n\n";
 //intro 2
 sleep(1);
 echo $putih."[".$kuning."!".$putih."]".$turkis."Login ";
@@ -30,10 +31,10 @@ $x=0;$c=5;while($x<$c){
  sleep(1);
 echo"•";
  }
-echo"\n";
+echo"\n\n";
 echo $ijo."Note : \n";
 echo $ijo."[ENG] Bots Are Running ... PLEASE WAIT .. \n";
-echo $ijo."[ENG] IF THERE IS NO RESPONSE FROM SERVER, THEN THE TOKEN OR CONFIG IS WRONG \n";
+echo $ijo."[ENG] IF THERE IS NO RESPONSE FROM SERVER, THEN THE TOKEN OR CONFIG IS WRONG \n\n";
 echo $ijo."[IDN] Bot akan jalan ... Tunggu Sebentar ... \n";
 echo $ijo."[IDN] Jika Tidak ada respon dari server, mohon di cek kembali token/confignya \n";
 require"cfg.php";
@@ -41,25 +42,25 @@ $urls[]=array("url"=>"http://api.coinclub.global/api/v1/user/getUserDetail?token
 unset($url);$jsonAuth=json_decode($responsx[0]["respons"],true);
 if($jsonAuth["errcode"]==1000&&!empty($jsonAuth["data"]["user"]["userName"])){
 echo $kuning."[".$putih."•".$kuning."]". $turkis." Informasi Akun \n";
-echo $putih."[".$turkis."*".$putih."] Balance : ".$ijo.$jsonAuth["data"]["totalBalance"];
-echo $putih."[".$turkis."*".$putih."] User Id : ".$ijo.$jsonAuth["data"]["user"]["userId"];
-echo $putih."[".$turkis."*".$putih."] User Name : ".$ijo.$jsonAuth["data"]["user"]["userName"];
+echo $putih."[".$turkis."*".$putih."] Balance : ".$ijo.$jsonAuth["data"]["totalBalance"].$t;
+echo $putih."[".$turkis."*".$putih."] User Id : ".$ijo.$jsonAuth["data"]["user"]["userId"].$t;
+echo $putih."[".$turkis."*".$putih."] User Name : ".$ijo.$jsonAuth["data"]["user"]["userName"].$t;
 
 echo $putih."[√] Login Success... ";
-echo"\n";
+echo"\n\n";
 
 }
 else{
  die("Config Kamu Salah.");
 }
 
- echo $putih."Bot akan jalan dalam 5 detik ";
+ echo $kuning."Bot akan jalan dalam 5 detik ";
  $a=0;$r=5;while($a<$r){
   $a++;
   sleep(1);
  echo ".";
   }
- echo "\n";
+ echo "\n\n";
 while(TRUE){
  $curl=curl_init();
  curl_setopt_array($curl,array(CURLOPT_URL=>"http://api.coinclub.global/api/v1/post/estimate",CURLOPT_RETURNTRANSFER=>true,CURLOPT_ENCODING=>"",CURLOPT_MAXREDIRS=>10,CURLOPT_TIMEOUT=>30,CURLOPT_HTTP_VERSION=>CURL_HTTP_VERSION_1_1,CURLOPT_CUSTOMREQUEST=>"POST",CURLOPT_POSTFIELDS=>"userId=".$config["userId"]."&postId=".rand(1079502,1113452)."&platform=1&accessKeyId=".$config["accessKeyId"]."&timestamp=1546954347645&versionCode=1.5.8&nonce=".$config["nonce"]."&version=1",CURLOPT_HTTPHEADER=>array("Accept-Encoding: gzip","Connection: Keep-Alive","Content-Length: 188","Content-Type: application/x-www-form-urlencoded","Host: api.coinclub.global","User-Agent: okhttp/3.9.0","token: ".$config["token"],"deviceId: ".$config["deviceId"],),));
@@ -77,12 +78,13 @@ while(TRUE){
    $view=$get["viewAward"];
    $rview=$get["rankViewAward"];
    $rtime=$get["readTimeAward"];
-   echo $putih."Reward Nonton : ".$ijo.$view;
+   echo $putih."[".$turkis."•".$putih."] Status : ".$ijo."Sukses ".$putih." || ".$putih."Code :$turkis 200". $putih."Reward Nonton : +".$ijo.$view;
 echo "\n";
 echo $putih."Rank View Reward: ".$ijo.$rview;
    echo "\n";
 echo $putih."Rank Time Reward: ".$ijo.$rtime;
    echo "\n";
+echo $
 sleep(1);
   }
 if($e->errcode=="500"){
