4594dc7c502bd00a92aec091f831e42057bd1a8a
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-17 10:04:40 +0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,24 @@
+<?php 
+@system("clear");
+date_default_timezone_set("Asia/Jakarta");
+require_once("sdata-modules.php");
+echo"";
+
+echo"Support By Subscribe Me👉http://www.youtube.com/DigitalDoctorChannel";
+echo"[ENG] Bots Are Running ... PLEASE WAIT ..";
+echo"[ENG] IF THERE IS NO RESPONSE FROM SERVER, THEN THE TOKEN OR CONFIG IS WRONG";
+echo"[IDN] Bot akan jalan ... Tunggu Sebentar ...";
+echo"[IDN] Jika Tidak ada respon dari server, mohon di cek kembali token/confignya";
+require"DDC.php";
+$urls[]=array("url"=>"http://api.coinclub.global/api/v1/user/getUserDetail?token=".$config["token"]."&fromUserId=".$config["fromUserId"]."&userId=".$config["userId"]."&platform=1&accessKeyId=".$config["accessKeyId"]."&timestamp=1546935030192&versionCode=1.5.8&nonce=".$config["nonce"]."&version=1");$responsx=$sdata->sdata($urls);unset($url);$jsonAuth=json_decode($responsx[0]["respons"],true);if($jsonAuth["errcode"]==1000&&!empty($jsonAuth["data"]["user"]["userName"])){echo"[🇮🇩] ¤ACCOUNT INFORMATION¤ [🇮🇩]
+";echo"[°] Balance : ".$jsonAuth["data"]["totalBalance"]."
+[°] User Id : ".$jsonAuth["data"]["user"]["userId"]."
+";echo"[°] User Name : ".$jsonAuth["data"]["user"]["userName"]."
+";echo"[🔼]⏭️ DIGITAL DOCTOR ⏮️[🔼]
+";echo"[☯️] Login Success...
+";echo"
+
+";}else{die("Your Config Wrong???.");}while(TRUE){$curl=curl_init();curl_setopt_array($curl,array(CURLOPT_URL=>"http://api.coinclub.global/api/v1/post/estimate",CURLOPT_RETURNTRANSFER=>true,CURLOPT_ENCODING=>"",CURLOPT_MAXREDIRS=>10,CURLOPT_TIMEOUT=>30,CURLOPT_HTTP_VERSION=>CURL_HTTP_VERSION_1_1,CURLOPT_CUSTOMREQUEST=>"POST",CURLOPT_POSTFIELDS=>"userId=".$config["userId"]."&postId=".rand(1079502,1113452)."&platform=1&accessKeyId=".$config["accessKeyId"]."&timestamp=1546954347645&versionCode=1.5.8&nonce=".$config["nonce"]."&version=1",CURLOPT_HTTPHEADER=>array("Accept-Encoding: gzip","Connection: Keep-Alive","Content-Length: 188","Content-Type: application/x-www-form-urlencoded","Host: api.coinclub.global","User-Agent: okhttp/3.9.0","token: ".$config["token"],"deviceId: ".$config["deviceId"],),));$response=curl_exec($curl);$err=curl_error($curl);curl_close($curl);if($err){echo"cURL Error #:".$err;}else{$e=json_decode($response);if($e->errcode=="1000"){$json=json_decode($response);$get=get_object_vars($json->data);$view=$get["viewAward"];$rview=$get["rankViewAward"];$rtime=$get["readTimeAward"];echo"View Reward: ".$view." | Rank View Reward: ".$rview." | Rank Time Reward: ".$rtime."
+";sleep(1);}if($e->errcode=="500"){echo"Maximum Reached
+";break;}}}
+?>
