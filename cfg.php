f361e1f380766737c55d0f6c6311810fe1267716
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-18 14:05:38 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+//Please Don't Rename This File//
+//jangan ganti nama file ini//
+
+$config['token']  = 'dc01e902bf0e460694e4b6c4cec2984a';
+$config['fromUserId'] = '1767752';
+$config['userId']  = '1767752';
+$config['accessKeyId']  = 'MoeCube-7cc405348a6b11e89bddc85b762def01';
+$config['nonce']  = '34c578af-b0d2-423d-9c4c-b56b581bd1de';
+$config['deviceId']  = 'cab4f993b03e17880123456789ABCDEF';
