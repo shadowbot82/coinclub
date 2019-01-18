c04d9287c5465e9e495f5b4f805eb94aa20e057d
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-18 14:02:06 +0700

    Delete DDC.php

--- DDC.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Please Don't Rename This File//
-
-$config['token']  = 'xxxxxxx';
-$config['fromUserId'] = 'xxxxxx';
-$config['userId']  = 'xxxxx';
-$config['accessKeyId']  = 'xxxxxx';
-$config['nonce']  = 'xxxxx';
-$config['deviceId']  = 'xxxxx';
